8e0cab8f920eacbcd8f13e71270605b4d42f2fb4
adamgemmell <gemmell.adam+github@gmail.com>
2018-02-18 17:29:56 +0000

    circle

--- src/com/ajsg2/question1/q1b.kt
+++ src/com/ajsg2/question1/q1b.kt
@@ -8,111 +8,127 @@ import org.lwjgl.opengl.*
 import kotlin.math.cos
 import kotlin.math.sin
 
-class q1b {
 
-    fun main(args: Array<String>) {
+fun main(args: Array<String>) {
 
-        // Set up GLFW window
+    // Set up GLFW window
 
-        val errorCallBack = GLFWErrorCallback.createPrint(System.err)
-        GLFW.glfwSetErrorCallback(errorCallBack)
-        GLFW.glfwInit()
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3)
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3)
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE)
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE)
-        val window: Long = GLFW.glfwCreateWindow(Constants.WIDTH, Constants.HEIGHT, "HelloGL", 0,
-                0)
-        GLFW.glfwMakeContextCurrent(window)
-        GLFW.glfwSwapInterval(1)
-        GLFW.glfwShowWindow(window)
+    val errorCallBack = GLFWErrorCallback.createPrint(System.err)
+    GLFW.glfwSetErrorCallback(errorCallBack)
+    GLFW.glfwInit()
+    GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3)
+    GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3)
+    GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE)
+    GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE)
+    GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE)
+    val window: Long = GLFW.glfwCreateWindow(Constants.WIDTH, Constants.HEIGHT, "HelloGL", 0, 0)
+    GLFW.glfwMakeContextCurrent(window)
+    GLFW.glfwSwapInterval(1)
+    GLFW.glfwShowWindow(window)
 
-        // Set up OpenGL
+    // Set up OpenGL
 
-        GL.createCapabilities()
-        GL11.glClearColor(0.2f, 0.4f, 0.6f, 0.0f)
-        GL11.glClearDepth(1.0)
+    GL.createCapabilities()
+    GL11.glClearColor(0.2f, 0.4f, 0.6f, 0.0f)
+    GL11.glClearDepth(1.0)
 
-        // Set up minimal shader programs
+    // Set up minimal shader programs
 
-        val vertexShader =
-                """#version 330
+    val vertexShader =
+            """#version 330
 
             in vec3 v;
             void main() {
                 gl_Position = vec4(v, 1.0);
             }""".trimMargin()
 
-        val fragmentShader =
-                """#version 330
+    val fragmentShader =
+            """#version 330
+
+            in vec3 position;
 
             out vec4 frag_color;
             void main() {
                 frag_color = vec4(1.0, 1.0, 1.0, 1.0);
             }""".trimMargin()
 
-        // Compile vertex shader
-        val vs = GL20.glCreateShader(GL20.GL_VERTEX_SHADER)
-        GL20.glShaderSource(vs, vertexShader)
-        GL20.nglCompileShader(vs)
+    // Compile vertex shader
+    val vs = GL20.glCreateShader(GL20.GL_VERTEX_SHADER)
+    GL20.glShaderSource(vs, vertexShader)
+    GL20.nglCompileShader(vs)
 
-        // Compile fragment shader
-        val fs = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER)
-        GL20.glShaderSource(fs, fragmentShader)
-        GL20.glCompileShader(fs)
+    // Compile fragment shader
+    val fs = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER)
+    GL20.glShaderSource(fs, fragmentShader)
+    GL20.glCompileShader(fs)
 
-        // Link vertex and fragment shaders into an active program
-        val program = GL20.glCreateProgram()
-        GL20.glAttachShader(program, vs)
-        GL20.glAttachShader(program, fs)
-        GL20.glLinkProgram(program)
-        GL20.glUseProgram(program)
+    // Link vertex and fragment shaders into an active program
+    val program = GL20.glCreateProgram()
+    GL20.glAttachShader(program, vs)
+    GL20.glAttachShader(program, fs)
+    GL20.glLinkProgram(program)
+    GL20.glUseProgram(program)
 
-        // Set up data
+    // Set up data
 
-        val numPoints = 360
+    val numPoints = 120
+    val radius = 0.5f
 
-        // Set up float buffer
-        val coords = FloatArray(numPoints*2, { i ->
-            if (i%2 == 0)
-                cos(2*Math.PI * i/(2*numPoints)).toFloat()
-            else
-                sin(2*Math.PI * (i-1)/(2*numPoints)).toFloat()} )
+    // Set up float buffer
+    val coords = FloatArray(numPoints*9, { i:Int ->
+        when (i % 9) {
+            0 -> {
+                val ithpoint = i/9;
+                radius*cos(2*Math.PI * ithpoint/numPoints).toFloat()
+            }
+            1 -> {
+                val ithpoint = (i-1)/9;
+                radius*sin(2*Math.PI * ithpoint/numPoints).toFloat()
+            }
+            3 -> {
+                val ithpoint = (i-3)/9+1
+                radius*cos(2*Math.PI * ithpoint/numPoints).toFloat()
+            }
+            4 -> {
+                val ithpoint = (i-4)/9+1;
+                radius*sin(2*Math.PI * ithpoint/numPoints).toFloat()
+            }
+            else -> 0f
+        }})
 
-        val fbo = BufferUtils.createFloatBuffer(coords.size)
-        fbo.put(coords)
-        fbo.flip()      // Mark ready for read
+    val fbo = BufferUtils.createFloatBuffer(coords.size)
+    fbo.put(coords)
+    fbo.flip()      // Mark ready for read
 
-        // Store into a vbo
-        val vbo = GL15.glGenBuffers()       // Get name for the VBO
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo)    // Activate
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, fbo, GL15.GL_STATIC_DRAW)   // Send to GPU
+    // Store into a vbo
+    val vbo = GL15.glGenBuffers()       // Get name for the VBO
+    GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo)    // Activate
+    GL15.glBufferData(GL15.GL_ARRAY_BUFFER, fbo, GL15.GL_STATIC_DRAW)   // Send to GPU
 
-        // Bind the vbo into a VAO
-        val vao = GL30.glGenVertexArrays()          // Get name for the VAO
-        GL30.glBindVertexArray(vao)                 // Activate
-        GL20.glEnableVertexAttribArray(0)       // Enable attribute 0
-        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0)
-        // Link VBO to VAO attr 0
+    // Bind the vbo into a VAO
+    val vao = GL30.glGenVertexArrays()          // Get name for the VAO
+    GL30.glBindVertexArray(vao)                 // Activate
+    GL20.glEnableVertexAttribArray(0)       // Enable attribute 0
+    GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0)
+    // Link VBO to VAO attr 0
 
-        // Loop
-        while (!GLFW.glfwWindowShouldClose(window)) {
-            GLFW.glfwPollEvents()
+    // Loop
+    while (!GLFW.glfwWindowShouldClose(window)) {
+        GLFW.glfwPollEvents()
 
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT or GL11.GL_DEPTH_BUFFER_BIT)
-            GL30.glBindVertexArray(vao)
-            GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3)
-            // Start at 0, num vertices 3
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT or GL11.GL_DEPTH_BUFFER_BIT)
+        GL30.glBindVertexArray(vao)
+        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, numPoints*3)
+        // Start at 0, num vertices 3
 
-            GLFW.glfwSwapBuffers(window)
-        }
-
-        // Clean up
-
-        GL15.glDeleteBuffers(vbo)
-        GL30.glDeleteVertexArrays(vao)
-        GLFW.glfwDestroyWindow(window)
-        GLFW.glfwTerminate()
-        GLFW.glfwSetErrorCallback(null).free()
+        GLFW.glfwSwapBuffers(window)
     }
+
+    // Clean up
+
+    GL15.glDeleteBuffers(vbo)
+    GL30.glDeleteVertexArrays(vao)
+    GLFW.glfwDestroyWindow(window)
+    GLFW.glfwTerminate()
+    GLFW.glfwSetErrorCallback(null).free()
 }
